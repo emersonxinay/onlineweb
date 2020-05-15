1d7fd856b3028c954f512149e09777b4f9f653b2
emerson espinoza <xinayespinoza@gmail.com>
2020-05-15 17:27:37 -0400

    precompile2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,12 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.initialize_on_precompile = false
   config.assets.compile = true
   # config.assets.precompile =  ['*.js', '*.css', '*.scss', '*.css.erb']
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
@@ -93,5 +95,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
 end
