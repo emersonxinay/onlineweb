10632e3b3a1a1bbf45246a673ae908cd478af9b1
emerson espinoza <xinayespinoza@gmail.com>
2020-05-16 03:22:23 -0400

    cambios para heroku

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,27 +2,27 @@ class Ability
   include CanCan::Ability
 
 # ....::::::: el segundo que probe :::::.......
-    # def initialize(user)
-    #   user ||= AdminUser.new
-    #
-    #   if user.role == 'admin'
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    # end
+    def initialize(user)
+      user ||= AdminUser.new
 
-# ::::::aqui esta el codigo que estuvo funcionando normal antes del 13 de mayo
-  
-  def initialize(user)
-    can :read, Home, public: true
-
-    if user.present?  # additional permissions for logged in users (they can read their own posts)
-      can :read, Home, user_id: user.id
-
-      if user.admin?  # additional permissions for administrators
-        can :read, Home
+      if user.role == 'admin'
+        can :manage, :all
+      else
+        can :read, :all
       end
     end
-  end
+
+# ::::::aqui esta el codigo que estuvo funcionando normal antes del 13 de mayo
+  #
+  # def initialize(user)
+  #   can :read, Home, public: true
+  #
+  #   if user.present?  # additional permissions for logged in users (they can read their own posts)
+  #     can :read, Home, user_id: user.id
+  #
+  #     if user.admin?  # additional permissions for administrators
+  #       can :read, Home
+  #     end
+  #   end
+  # end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
   config.assets.compile = true
   # config.assets.precompile =  ['*.js', '*.css', '*.scss', '*.css.erb']
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
